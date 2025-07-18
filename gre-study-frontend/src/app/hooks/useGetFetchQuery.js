3f8588c86cff8ce6e636fa7d526d86aab870bea7
AlexBaratta <alexbaratta1@gmail.com>
2025-07-18 21:50:19 +0200

    Remove unused custom hook for fetching query data

--- gre-study-frontend/src/app/hooks/useGetFetchQuery.js
+++
@@ -1,6 +0,0 @@
-import { useQueryClient } from "@tanstack/react-query";
-
-export default function useGetFetchQuery(key){
-    const queryClient = useQueryClient();
-    return queryClient.getQueryData(key);
-}
