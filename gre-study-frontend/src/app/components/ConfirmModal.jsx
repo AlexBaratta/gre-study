334e2fabdc4784e1480a9422bfd39b685a3b0731
AlexBaratta <alexbaratta1@gmail.com>
2025-07-31 18:39:57 +0200

    Finish confirm modal

--- gre-study-frontend/src/app/components/ConfirmModal.jsx
+++ gre-study-frontend/src/app/components/ConfirmModal.jsx
@@ -17,48 +17,34 @@ export default function ConfirmModal({
   return (
     <Dialog open={visible} onClose={onCancel} className="relative z=50">
       <DialogBackdrop className="fixed inset-0 bg-gray-500/30 backdrop-blur-xs" />
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <DialogPanel
-            transition
-            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
-          >
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-              <div className="sm:flex sm:items-start">
-                <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
-                  <ExclamationTriangleIcon
-                    aria-hidden="true"
-                    className="size-6 text-red-600"
-                  />
-                </div>
-                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                  <DialogTitle
-                    as="h3"
-                    className="text-base font-semibold text-gray-900"
-                  >
-                    {title}
-                  </DialogTitle>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">{message}</p>
-                  </div>
+      <div className="fixed inset-0 flex items-center justify-center">
+        <div className="relative justify-center text-center">
+          <DialogPanel className="overflow-hidden max-w-lg bg-white rounded-lg">
+            <div className="flex items-start p-6">
+              <div className="flex size-12 items-center justify-center rounded-full bg-red-100 shrink-0">
+                <ExclamationTriangleIcon className="size-6 text-red-600" />
+              </div>
+              <div className="ml-4 text-left">
+                <DialogTitle as="h3" className="text-lg font-semibold">
+                  {title}
+                </DialogTitle>
+                <div className="mt-2 text-sm text-gray-500">
+                  <p>{message}</p>
                 </div>
               </div>
             </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            <div className="flex justify-end bg-gray-50 px-4 py-3 gap-2">
               <button
-                type="button"
-                onClick={onConfirm}
-                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto"
-              >
-                Delete
-              </button>
-              <button
-                type="button"
-                data-autofocus
+                className="bg-white border border-gray-300 rounded px-3 py-2 hover:bg-gray-100"
                 onClick={onCancel}
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
               >
                 Cancel
+              </button>
+              <button
+                className="bg-red-600 rounded px-3 py-2 text-white font-semibold hover:bg-red-500"
+                onClick={onConfirm}
+              >
+                Confirm
               </button>
             </div>
           </DialogPanel>
